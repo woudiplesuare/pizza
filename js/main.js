555815e3cd1ce4efbb622f92d39b962607c242ab
woudiplesuare <72888864+woudiplesuare@users.noreply.github.com>
2020-12-21 22:15:01 +0300

    Pizza new files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    $(window).scroll(function() {
+        $('.container').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }
+        });
+    });
+    $(window).scroll(function() {
+        $('.contact__container').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInUp");
+            }
+        });
+    });
+}) 
